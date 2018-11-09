2539dfe936e566264e43e5061c6ac651b6d7fee6
Diego Tubello <dtubello@baufest.com>
2018-11-09 18:55:10 -0300

    ejemplos y práctica

---
+++ Ejemplos/2_API/Baufest.NHibernate/Baufest.NHibernate.Api/Controllers/ProductoController.cs
@@ -0,0 +1,36 @@
+﻿using Baufest.NHibernate.Dominio.Entidades;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Baufest.NHibernate.Api.Controllers
+{
+    public class ProductoController : ApiController
+    {
+        // GET: api/Producto
+        public IEnumerable<Producto> Get()
+        {
+            return null;
+        }
+
+        // GET: api/Producto/5
+        public Producto Get(int id)
+        {
+            return null;
+        }
+
+        // POST: api/Producto
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Producto/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Producto/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Ejemplos/2_API/Baufest.NHibernate/Baufest.NHibernate.Repository/Mappings/Database.cs
@@ -0,0 +1,27 @@
+﻿using Baufest.NHibernate.Dominio.Entidades;
+using FluentNHibernate.Automapping;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+
+namespace Baufest.NHibernate.Repository.Mappings
+{
+    public static class Database
+    {
+        public static ISessionFactory CreateSessionFactory()
+        {
+            return Fluently
+                 .Configure()
+                 .Database(                
+                        MsSqlConfiguration.MsSql2012.ConnectionString(x => x.FromConnectionStringWithKey("IntroNH")))
+                 .Mappings(m => m.AutoMappings.Add(
+                                    AutoMap.AssemblyOf<Producto>()
+                                        .Where(t => t.Namespace == typeof(Producto).Namespace)))
+                 .ExposeConfiguration(cfg => new SchemaUpdate(cfg).Execute(false, true))
+                 .BuildSessionFactory();
+        }
+
+
+    }
+}
