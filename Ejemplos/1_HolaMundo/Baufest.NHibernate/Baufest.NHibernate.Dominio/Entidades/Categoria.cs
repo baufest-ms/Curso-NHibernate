6658d68fd8a85bfc2f33435a7984ada65f0c1c9c
Diego Tubello <dtubello@baufest.com>
2018-11-09 18:10:51 -0300

    Ejemplo del ejercicio de la clase 1

---
+++ Ejemplos/1_HolaMundo/Baufest.NHibernate/Baufest.NHibernate.Dominio/Entidades/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baufest.NHibernate.Dominio.Entidades
+{
+    public class Categoria
+    {
+        public virtual int Id {get; set;}
+
+        public virtual string Nombre { get; set; }
+    }
+}
---
+++ Ejemplos/1_HolaMundo/Baufest.NHibernate/Baufest.NHibernate.Dominio/Entidades/Producto.cs
@@ -0,0 +1,15 @@
+﻿namespace Baufest.NHibernate.Dominio.Entidades
+{
+    public class Producto
+    {
+        public virtual int Id { get; set; }
+
+        public virtual string Nombre { get; set; }
+
+        public virtual string Descripcion { get; set; }
+
+        public virtual decimal Precio { get; set; }
+
+        public virtual Categoria Categoria { get; set; }
+    }
+}
