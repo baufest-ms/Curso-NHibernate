84e7cf02ffc461ec99f4084009c06975f7154e81
Diego Tubello <dtubello@baufest.com>
2018-11-12 11:51:05 -0300

    ejemplo

---
+++ Ejemplos/2_1_HolaMundo_Config/Baufest.NHibernate/Baufest.NHibernate.HolaMundo/Program.cs
@@ -0,0 +1,46 @@
+﻿using Baufest.NHibernate.Dominio.Entidades;
+using FluentNHibernate.Automapping;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+
+namespace Baufest.NHibernate.HolaMundo
+{
+    class Program
+    {
+        public static ISessionFactory CrearSessionFactory()
+        {         
+            return Fluently
+                 .Configure()
+                 .Database(
+                     MsSqlConfiguration.MsSql2012.ConnectionString(
+                            x => x.FromConnectionStringWithKey("IntroNH")))
+                 .Mappings(m => m.AutoMappings.Add(
+                            AutoMap.AssemblyOf<Producto>()
+                                .Where(t => t.Namespace == typeof(Producto).Namespace)))
+                 .ExposeConfiguration(cfg => new SchemaUpdate(cfg).Execute(true, true))
+                 .BuildSessionFactory();
+        }
+
+        static void Main(string[] args)
+        {
+            var sessionfactory = CrearSessionFactory();
+            using(var session = sessionfactory.OpenSession())
+            {
+                var categoria = new Categoria { Nombre = "Notebooks" };
+                session.Save(categoria);
+
+                var producto = new Producto
+                {
+                    Nombre = "Lenovo T470",
+                    Descripcion = "Laptop Lenovo T470 Core i5, 16GB RAM, SSD 128GB",
+                    Precio = 500,
+                    Categoria = categoria
+                };
+
+                session.Save(producto);
+            }
+        }
+    }
+}
