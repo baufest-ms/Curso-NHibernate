31ac7b8f185acb1680746bd58861fb92de19eaac
Diego Tubello <dtubello@baufest.com>
2018-12-11 10:09:25 -0300

    solución base de ejercicio intregrador

---
+++ Practica/EjercicioIntegrador/Baufest.NHibernate.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Baufest.NHibernate.Api.WebApiApplication" Language="C#" %>
---
+++ Practica/EjercicioIntegrador/Baufest.NHibernate.Repository/Mappings/Database.cs
@@ -0,0 +1,34 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+using System;
+
+namespace Baufest.NHibernate.Repository.Mappings
+{
+    public static class Database
+    {
+        private static readonly Lazy<ISessionFactory> sessionFactory = new Lazy<ISessionFactory>(CreateSessionFactory);
+
+        public static ISessionFactory SessionFactory
+        {
+            get { return sessionFactory.Value; }
+        }
+
+        private static ISessionFactory CreateSessionFactory()
+        {
+            return Fluently
+                 .Configure()
+                 .Database(                
+                        MsSqlConfiguration.MsSql2012.ConnectionString(x => x.FromConnectionStringWithKey("EjercicioNH")))
+                 //.Mappings(m => m.AutoMappings.Add(
+                 //                   AutoMap.AssemblyOf<>()
+                 //                       .Where(t => t.Namespace == typeof().Namespace)))
+
+                 .ExposeConfiguration(cfg => new SchemaUpdate(cfg).Execute(false, true))
+                 .BuildSessionFactory();
+        }
+
+
+    }
+}
