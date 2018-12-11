e0716ca7515423c3124e238bb9df7e1f21511ac1
Diego Tubello <dtubello@baufest.com>
2018-12-11 10:16:35 -0300

    ejercicio integrador para resolver

---
+++ Ejemplos/6_Ejercicio_Integrador/Baufest.NHibernate.Api/Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using NSwag.AspNet.Owin;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace Baufest.NHibernate.Api
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            RouteTable.Routes.MapOwinPath("swagger", app =>
+            {
+                app.UseSwaggerUi3(typeof(WebApiApplication).Assembly, settings =>
+                {
+                    settings.MiddlewareBasePath = "/swagger";
+                    settings.GeneratorSettings.Title = "NHibernate";
+                });
+            });
+        }
+    }
+}
